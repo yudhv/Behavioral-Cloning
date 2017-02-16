ea5b049ec19135532f13cad077ecb969045a77c2
Yudhvir Singh <ys13k1994@gmail.com>
2017-02-16 12:01:42 +0530

    Cross the dirt road ahead of the bridge

--- model.py
+++ model.py
@@ -27,7 +27,7 @@ def load_data(data):
 		im = misc.imread(data[i])
 		im = misc.imresize(im,size=DOWNSAMPLE_RATIO)
 		im = crop(im)
-		im = gray(im)
+		im = color_change(im)
 		temp.append(im)
 	return temp
 
@@ -38,9 +38,9 @@ def normalize(data):
     greyscale_max=255
     return a + ( ( (data - greyscale_min)*(b - a) )/(greyscale_max - greyscale_min))
 
-def gray(data):
+def color_change(data):
 	x = cv2.cvtColor(data,cv2.COLOR_BGR2YUV)
-	x = x[:,:,0:1]
+	# x = x[:,:,0:1]
 	return x
 
 def crop(data):
@@ -66,18 +66,23 @@ def flip(xdata,ydata):
 
 def set_model():
 	model = Sequential()
-	model.add(Convolution2D(36,5,5,border_mode='valid',activation="elu",
-		input_shape=X_train[0].shape), trainable="False")
-	model.add(Convolution2D(48,3,3,activation="elu",border_mode='valid'),trainable="False")
-	model.add(Convolution2D(64,3,3,activation="elu",border_mode='valid'),trainable="False")
-	model.add(Convolution2D(64,3,3,activation="elu",border_mode='valid'))
-	model.add(Flatten())
-	# model.add(Dropout(0.5))
-	# model.add(Dense(300, activation="elu"))
-	model.add(Dense(100, activation="elu"))
-	model.add(Dense(50, activation="elu"))
-	model.add(Dense(10, activation="elu"))
-	model.add(Dense(1, activation="linear"))
+	model.add(Convolution2D(3, 1, 1, border_mode='same', name='color_conv',
+		input_shape=X_train[0].shape, trainable = 'False'))
+	model.add(Convolution2D(36,5,5,border_mode='same',activation="elu",
+	 name='conv1', trainable = 'False'))
+	model.add(Convolution2D(48,3,3,activation="elu",border_mode='same',
+	 name='conv2', trainable = 'False'))
+	model.add(Convolution2D(64,3,3,activation="elu",border_mode='same', 
+		name='conv3', trainable = 'False'))
+	model.add(Convolution2D(64,3,3,activation="elu",border_mode='same', name='conv4'))
+	model.add(Flatten(name='flat1'))
+	# model.add(Dropout(0.2))
+	# model.add(Dense(1164, activation="elu"))
+	model.add(Dropout(.3, name='drop1'))
+	model.add(Dense(100, activation="elu", name='dense1'))
+	model.add(Dense(50, activation="elu", name='dense2'))
+	model.add(Dense(10, activation="elu", name='dense3'))
+	model.add(Dense(1, activation="linear", name='dense4'))
 	return model
 
 def for_drive(im):
@@ -85,13 +90,15 @@ def for_drive(im):
 	x = im
 	x = misc.imresize(x,size=DOWNSAMPLE_RATIO)
 	x = crop(x)
-	x = gray(x)
+	x = color_change(x)
 	x = normalize(x)
 	return x
 # columns = ['center', 'left', 'right', 'steering','throttle','brake','speed']
 if __name__ == "__main__":
 	log = pd.read_csv(FOLDER+"driving_log.csv")
-	# print(type(load_data((for s in log.ix[:,1:2].values[:100]: return s)))
+	# plt.hist(log.ix[:,3:4].values)
+	# plt.show()
+	# exit()
 	start = time.time()
 
 	# Load data
@@ -109,23 +116,22 @@ if __name__ == "__main__":
 	# X_train,y_train = curve_focus(X_train,y_train)
 	X_train,y_train = flip(X_train,y_train)
 	X_train = np.array(X_train)
-	# plt.hist(y_train)
-	# plt.show()
 	X_train = normalize(X_train)
 	
 	# Splitting data
 	print(X_train.shape)
 	X_train,X_validation,y_train,y_validation = train_test_split(X_train,y_train,
-		test_size=0.15,random_state=0)
+		test_size=0.2,random_state=0)
 	print(X_train.shape)
 
-	# model = set_model()
-	model = load_model('model1.h5') 
+	model = set_model()
+	model.load_weights('weights.h5',by_name=True) 
 
 	adam = Adam(lr=Learning_Rate)
-	model.compile(optimizer = adam, loss = 'mean_squared_error', 
-		metrics = ['mean_squared_error'])
+	model.compile(optimizer = adam, loss = 'mean_squared_error')
 	history = model.fit(X_train, y_train, batch_size=30, nb_epoch=EPOCHS, 
 		validation_data=(X_validation,y_validation))
+	model.save_weights('weights1.h5')
 	model.save('model1.h5')
+	
 	print("Model saved")
